f56ef3071153072bdb0a6a03fae420d93492f77f
SarvagnaGudlavalleti <92788333+SarvagnaGudlavalleti@users.noreply.github.com>
2021-11-05 21:52:34 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const mysql= require("mysql");
+const express= require("express");
+const bodyParser=require("body-parser");
+
+var app= express();
+app.use(bodyParser.json());
+var mysqlConnection = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "password",
+    database : "Curenet",
+    multipleStatements : true
+
+});
+mysqlConnection.connect((err) =>{
+    if(!err)
+    {
+        console.log("Connected");
+    }
+    else
+    {
+        console.log("Connection Failed");
+    }
+})
+app.listen (3000);
